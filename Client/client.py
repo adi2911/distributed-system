9ff7b0c3a990945bf446009fd337fb01bba9f3ef
adjohari <adjohari25@gmail.com>
2024-10-26 14:35:09 +0100

    add structure of project, add proto files

---
+++ Client/client.py
@@ -0,0 +1,19 @@
+
+
+class Client:
+    def __init__(self):
+        pass
+
+    def RPC_init():
+        pass
+    def RPC_lock_acquire():
+        pass
+    def RPC_lock_release():
+        pass
+    def append_file():
+        pass
+    def RPC_close():
+        pass
+
+
+#User prompt for file name and what to append
---
+++ Server/server.py
@@ -0,0 +1,14 @@
+
+#Needs to implemented multi threaded
+
+class Server:
+    def __init__(self):
+        pass
+    def lock_acquire():
+        pass
+    def lock_release():
+        pass
+    def append_file():
+        pass
+    def close():
+        pass
