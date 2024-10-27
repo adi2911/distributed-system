a66cf517d211fb441fdc02c8be46f4a5bf6e4566
Him0302 <81606642+Him0302@users.noreply.github.com>
2024-10-27 16:04:47 +0000

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -1,9 +1,19 @@
+from concurrent import futures
+
+import grpc
+import bidirectional.bidirectional_pb2_grpc as bidirectional_pb2_grpc
 
 #Needs to implemented multi threaded
 
 class Server:
     def __init__(self):
         pass
+
+    def init():
+       server.add_insecure_port('[::]:50051')
+       server.start()
+       server.wait_for_termination()
+
     def lock_acquire():
         pass
     def lock_release():
