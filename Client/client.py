50e5af6b6b0f66869de29d9fb6e7c6aab4b0d978
Him0302 <81606642+Him0302@users.noreply.github.com>
2024-10-27 12:28:02 +0000

    Update client.py

--- Client/client.py
+++ Client/client.py
@@ -1,11 +1,28 @@
+from __future__ import print_function
 
+import grpc
+import bidirectional.bidirectional_pb2_grpc as bidirectional_pb2_grpc
+import bidirectional.bidirectional_pb2 as bidirectional_pb2
 
 class Client:
     def __init__(self):
         pass
 
-    def RPC_init():
-        pass
+def send_message(stub,client_id):
+    request = bidirectional_pb2.ClientRequest(id=client_id)
+    response = stub.client_init(request)
+    print("The ID of the client is %s" % response.message)
+
+
+def RPC_init(int):
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = bidirectional_pb2_grpc.LockServiceStub(channel)
+        send_message(stub) #function to be made by us
+
+
+if __name__ == '__main__':
+    RPC_init(1)
+    
     def RPC_lock_acquire():
         pass
     def RPC_lock_release():
