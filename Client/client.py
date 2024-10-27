1b73c1489b41a77329074ef79eba5df594241bb2
Him0302 <81606642+Him0302@users.noreply.github.com>
2024-10-27 16:04:16 +0000

    Update client.py

--- Client/client.py
+++ Client/client.py
@@ -7,22 +7,18 @@ import bidirectional.bidirectional_pb2 as bidirectional_pb2
 class Client:
     def __init__(self):
         pass
-
-def send_message(stub,client_id):
-    request = bidirectional_pb2.ClientRequest(id=client_id)
-    response = stub.client_init(request)
-    print("The ID of the client is %s" % response.message)
-
-
-def RPC_init(int):
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = bidirectional_pb2_grpc.LockServiceStub(channel)
-        send_message(stub) #function to be made by us
-
-
-if __name__ == '__main__':
-    RPC_init(1)
     
+    def send_message(stub,client_id):
+        request = bidirectional_pb2.lock_args(id=client_id)
+        response = stub.client_init(request)
+        print("The ID of the client is %s" % response.message)
+
+
+    def RPC_init(int):
+        with grpc.insecure_channel('localhost:50051') as channel:
+            stub = bidirectional_pb2_grpc.LockServiceStub(channel)
+            send_message(stub) #function to be made by us
+
     def RPC_lock_acquire():
         pass
     def RPC_lock_release():
@@ -31,6 +27,9 @@ if __name__ == '__main__':
         pass
     def RPC_close():
         pass
+
+
+#User prompt for file name and what to append
 
 
 #User prompt for file name and what to append
